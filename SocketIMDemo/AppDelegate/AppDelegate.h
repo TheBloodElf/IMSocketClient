df11c6a056ec923d0f0cddffefd076495286b8fe
TomLongJoy <471727371@qq.com>
2018-04-23 10:16:08 +0800

    add new managet
    test add new user managet zlj_TomLongJoy

--- SocketIMDemo/AppDelegate/AppDelegate.h
+++ SocketIMDemo/AppDelegate/AppDelegate.h
@@ -5,7 +5,9 @@
 //  Created by 李勇 on 18/3/15.
 //  Copyright (c) 2018年李勇. All rights reserved.
 //
-
+/**
+ Add new user manager zlj_TomLongJoy！！Hello Everyone
+ */
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
